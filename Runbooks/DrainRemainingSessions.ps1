1ee0650f8353c4c9cd9931865b8db4caba2a09a0
Anthony Watherston <anwather@microsoft.com>
2021-03-22 16:40:45 +1100

    Draining script

--- Runbooks/DrainRemainingSessions.ps1
+++ Runbooks/DrainRemainingSessions.ps1
@@ -9,3 +9,17 @@ Param(
 . .\CommonFunctions.ps1
 
 $connectionString = Get-AutomationVariable -Name "ConnectionString"
+
+$rows = Get-AllCurrentStatus -ConnectionString $connectionString -TableName status -HostPoolName $params.HostPoolName
+
+foreach ($row in $rows) {
+    if ($row.status -eq "New") {
+        Write-Output "Enabling drain on $($row.RowKey)"
+        Update-AzWvdSessionHost -HostPoolName $HostPoolName -ResourceGroupName $ResourceGroupName -Name $row.RowKey -AllowNewSession:$false
+        Update-Status -ConnectionString $connectionString `
+            -TableName status `
+            -HostPoolName $HostPoolName `
+            -Status "Draining" `
+            -SessionHostName $row.RowKey
+    }
+}
